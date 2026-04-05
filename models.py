292290dd73080d1ffc3afb1e2c51f2da6e324c6e
Blent User <user@blent>
2026-04-05 14:06:17 +0000

    premier commit

---
+++ models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Initialisation de l'extension SQLAlchemy
+db = SQLAlchemy()
+
+# Définition des modèles
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    id = db.Column(db.String(10), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, default=0)
+    
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Relation avec les éléments du panier
+    items = db.relationship('CartItem', backref='cart', lazy=True, cascade='all, delete-orphan')
+    
+    def __repr__(self):
+        return f'<Cart {self.id}>'
+
+class CartItem(db.Model):
+    __tablename__ = 'cart_items'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('carts.id'), nullable=False)
+    product_id = db.Column(db.String(10), db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, default=1)
+    
+    # Relation avec le produit
+    product = db.relationship('Product', backref='cart_items')
+    
+    def __repr__(self):
+        return f'<CartItem {self.id}, Product: {self.product_id}, Qty: {self.quantity}>'
