b23e4a5f46ee5f04ff5ff277838283787fd545a9
Blent User <user@blent>
2026-04-06 16:39:26 +0000

    inscription d'un nouvel utilisateur

--- connection.py
+++ connection.py
@@ -6,11 +6,26 @@ JWT_SECRET = "d3fb12750c2eff92120742e1b334479e"
 
 app = Flask(__name__)
 
+@app.route('/api/auth/register', methods=["POST"])
+def connection_and_generate_token():
+    body = request.get_json()
+    id = body.get("id", "")
+    statutDuDemandeur = body.get("statut")
+    creerClient = body.get("client")
+    creerAdministrateur = body.get("administrator")
+    typeDeCompte = 'le client' if user.statut == "client" else "l'administrateur"
+    password = request.headers.get("Password", "0")
+    user = User
+    create_user(User(id, password, statutDuDemandeur, creerClient, creerAdministrateur))
+
+
+
 @app.route('/api/auth/login', methods=["POST"])
 def connection_and_generate_token():
     body = request.get_json()
     id = body.get("id", "")
     statut = body.get("statut")
+    typeDeCompte = 'le client' if user.client else "l'administrateur"
     password = request.headers.get("Password", "0")
     if authenticate(id, password):
         token = jwt.encode(
@@ -21,6 +36,6 @@ def connection_and_generate_token():
             JWT_SECRET,
             algorithm="HS256"
         )
-        return jsonify({"token": token}), 200
+        return jsonify({"token": token + "pour " + typeDeCompte + "id=" + id}), 200
     else:
         return jsonify({"error": "Identifiant/Mot de passe invalides."}), 401
